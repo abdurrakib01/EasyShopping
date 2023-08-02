c0d1759deaab2e7af677f8f849760aae8df13426
Abdur Rakib <rakib15-13656@diu.edu.bd>
2023-08-02 15:51:24 +0600

    Update settings.py

--- EasyShoping/settings.py
+++ EasyShoping/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'components.apps.ComponentsConfig',
     'authentication.apps.AuthenticationConfig',
-    'EasyShopping',
+    'EasyShoping',
 ]
 
 MIDDLEWARE = [
